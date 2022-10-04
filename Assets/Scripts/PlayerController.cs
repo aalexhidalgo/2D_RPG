2576f5b692bb2350d2b44617766affbb155e048a
Alexander Hidalgo <alexhidalgocalafat@gmail.com>
2022-10-04 10:39:19 +0200

    4/10/2022

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    public const string HORIZONTAL = "Horizontal", VERTICAL = "Vertical";
+    private float inputTol = 0.2f; //Input tolerance
+    private float xInput, yInput;
+
+    // Update is called once per frame
+    void Update()
+    {
+        xInput = Input.GetAxisRaw(HORIZONTAL);
+
+        if(Mathf.Abs(xInput) > inputTol)
+        {
+            Vector3 translation = new Vector3(xInput * speed * Time.deltaTime, 0, 0);
+            transform.Translate(translation);
+        }
+
+        yInput = Input.GetAxisRaw(VERTICAL);
+
+        if (Mathf.Abs(yInput) > inputTol)
+        {
+            Vector3 translation = new Vector3(0, yInput * speed * Time.deltaTime, 0);
+            transform.Translate(translation);
+        }
+    }
+}
