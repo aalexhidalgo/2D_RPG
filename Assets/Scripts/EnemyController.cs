022869116815c752e8061bf93ce779b2da04c5b2
Alexander Hidalgo <alexhidalgocalafat@gmail.com>
2022-11-07 13:39:55 +0100

    h

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     public float speed = 1f;
-    public Vector2 directionToMove;
+    private Vector2 directionToMove;
 
     private Rigidbody2D _rigidbody;
     private bool isMoving;
@@ -20,11 +20,20 @@ public class EnemyController : MonoBehaviour
     //Time the enemy has been taking a step
     private float timeToMakeStepCounter;
 
+    [Tooltip("If enemy movement is not random, ememyDirections needs to have at least two elements")]
+    [SerializeField] private bool hasRandomMove;
+    [Tooltip("Directions the enemy will follow to complete a path. The idea is that it should be cyclical.Components must be - 1, 0 or 1")]
+    [SerializeField] private Vector2[] enemyDirections;
+    private int indexDirection;
+
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
-        timeBetweenStepsCounter = timeBetweenSteps;
-        timeToMakeStepCounter = timeToMakeStep;
+        timeBetweenStepsCounter = timeBetweenSteps * (hasRandomMove ? Random.Range (0.5f, 1.5f) : 1);
+        timeToMakeStepCounter = timeToMakeStep * (hasRandomMove ? Random.Range(0.5f, 1.5f) : 1);
+
+        indexDirection = 0;
+        directionToMove = hasRandomMove ? new Vector2(Random.Range(-1, 2), Random.Range(-1, 2)) : enemyDirections[indexDirection];
     }
 
     void Update()
@@ -40,14 +49,28 @@ public class EnemyController : MonoBehaviour
                 _rigidbody.velocity = Vector2.zero;
             }
         }
-        else
+        else //No movement
         {
             timeBetweenStepsCounter -= Time.deltaTime;
             if(timeBetweenStepsCounter < 0)
             {
                 isMoving = true;
                 timeToMakeStepCounter = timeToMakeStep;
-                directionToMove = new Vector2(Random.Range(-1, 2), Random.Range(-1, 2));
+
+                //It has random movement
+                if (hasRandomMove)
+                {
+                    directionToMove = new Vector2(Random.Range(-1, 2), Random.Range(-1, 2));
+                }
+            }
+            else
+            {
+                indexDirection++;
+                if(indexDirection >= enemyDirections.Length)
+                {
+                    indexDirection = 0;
+                }
+                directionToMove = enemyDirections[indexDirection];
             }
         }
 
