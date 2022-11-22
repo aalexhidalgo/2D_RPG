e4e15d57c17b7370a1de250568e6d5726f06aa51
Alexander Hidalgo <alexhidalgocalafat@gmail.com>
2022-11-22 08:39:02 +0100

    22/11/2022

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -12,6 +12,11 @@ public class UIManager : MonoBehaviour
     public void ToggleInventory()
     {
         inventoryPanel.SetActive(!inventoryPanel.activeInHierarchy); //If it's active it returns true and if it's false it returns false
+        if (inventoryPanel.activeInHierarchy)
+        {
+            ClearInventory();
+            FillInventory();
+        }
     }
 
     private void Update()
@@ -19,6 +24,28 @@ public class UIManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.I))
         {
             ToggleInventory();
+        }
+    }
+
+    public void FillInventory()
+    {
+        WeaponManager weaponManagerScript = FindObjectOfType<WeaponManager>();
+        List<GameObject> weapons = weaponManagerScript.GetAllWeapons();
+        foreach(GameObject w in weapons)
+        {
+            Button button = Instantiate(weaponButton, inventoryPanel.transform);
+            button.onClick.AddListener(() => weaponManagerScript.ChangeWeapon(w.GetComponent<WeaponDamage>().index));
+            button.image.sprite = w.GetComponent<SpriteRenderer>().sprite;
+
+        }
+
+    }
+
+    public void ClearInventory()
+    {
+        foreach (Transform item in inventoryPanel.transform)
+        {
+            Destroy(item.gameObject);
         }
     }
 }
